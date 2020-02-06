a29f63caaa2f7c4d3b7016039e97f3f48f473e48
estitesc <evan@teespring.com>
2020-02-06 15:38:09 -0500

    Styles initial on install page and modal

--- js/oninstall.js
+++ js/oninstall.js
@@ -5,17 +5,21 @@ $(() => {
 function init() {
 
   var choice = async () => {
-    const {test} = await Swal.fire({
+    Swal.fire({
       title: 'Go to Uber.com and sign in as a rider before clicking continue.',
       text: 'Once you are logged in, press continue below. Then you can view your Uber history and offset information by clicking the UberNeutral chrome extension link in the top right.',
       confirmButtonText: 'Continue &rarr;',
-      // showCancelButton: true
-    });
-    window.open("https://riders.uber.com/trips", "_blank");
+      showCancelButton: true,
+      confirmButtonColor: '#000000',
+      cancelButtonColor: '#ccc',
+    }).then((result) => {
+      if (result.value) {
+        window.open("https://riders.uber.com/trips", "_blank");
+      }
+    })
   };
 
   $("#continue").on('click', (ev) => {
-    // window.open("https://riders.uber.com/trips", "_blank");
     console.log('h');
     choice();
   });
